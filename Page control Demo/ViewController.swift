52c0c7e6c487aafc674338d3781ae743dc66bb58
Zenil <zenil.lakhani@internal.mail>
2022-05-17 14:13:11 +0530

    Initial Commit

---
+++ Page control Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Page control Demo
+//
+//  Created by MAC241 on 17/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
